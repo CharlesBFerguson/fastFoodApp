e343169a76a92e94de452798f7f0e0bc20cead2a
CharlesBFerguson <fergurburger326@gmail.com>
2020-03-19 08:15:20 -0400

    menu object added

--- placeOrder.js
+++ placeOrder.js
@@ -1,5 +1,5 @@
 function placeOrder() {
-  var customerOrder = document.getElementById("orderInput").value;
+  let customerOrder = document.getElementById("orderInput").value;
   console.log(`You have ordered ${customerOrder}`);
   document.getElementById("orderRecap").innerHTML = `You have ordered ${customerOrder}`;
   document.getElementById("orderInput").value = "";
@@ -9,15 +9,27 @@ function orderRecapClear() {
   document.getElementById("orderRecap").innerHTML = "";
 }
 
+let menu = {
+  burger:100,
+  cheeseburger:100,
+  hotdog: 100,
+  nuggets:1000,
+  fries:100,
+  tots:100,
+  shake:100,
+  soda:100,
+  water:100
+}
 
-// var p1 = 5;
-// var p2 = 7;
+
+// let p1 = 5;
+// let p2 = 7;
 
 // document.getElementById("placeOrder").addEventListener("click", function() {
 //   myFunction(p1, p2);
 // });
 
 // function myFunction(a, b) {
-//   var result = a * b;
+//   let result = a * b;
 //   document.getElementById("recap").innerHTML = `You have ordered ${result}`;
 // }
