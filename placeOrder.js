d5872de5d606d19a705044bbd099a5f1380e85a4
CharlesBFerguson <fergurburger326@gmail.com>
2020-03-24 09:08:39 -0400

    not sure

--- placeOrder.js
+++ placeOrder.js
@@ -3,7 +3,7 @@ function placeOrder() {
   // console.log(customerOrder.trim());
   console.log(customerOrder.split(","));
   console.log(customerOrder.split(" ").join(""));
-  // console.log(customerOrder.trim());
+  console.log(customerOrder);
   
   // console.log(`You have ordered ${customerOrder}`);
   document.getElementById("orderRecap").innerHTML = `You have ordered ${customerOrder}`;
