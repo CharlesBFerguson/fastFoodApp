07e353575523dcab493936700ad18c2dd84598f7
CharlesBFerguson <fergurburger326@gmail.com>
2020-03-26 07:05:11 -0400

    added .split/.trim from arrays2.js - .push added and needs to be tied-in

--- placeOrder.js
+++ placeOrder.js
@@ -1,9 +1,14 @@
 function placeOrder() {
-  let customerOrder = document.getElementById("orderInput").value;
-  // console.log(customerOrder.trim());
-  console.log(customerOrder.split(","));
-  console.log(customerOrder.split(" ").join(""));
-  console.log(customerOrder);
+  let customerOrder = document.getElementById("orderInput").value; // String
+  customerOrder = customerOrder.toLowerCase(); // String
+  customerOrder = customerOrder.split(','); //now Array - can chain .split above instead
+  customerOrder = customerOrder.join(''); // now String
+  customerOrder = customerOrder.split(' '); //now Array
+  console.log(typeof customerOrder, customerOrder);
+
+  customerOrder = customerOrder.join(':'); // now String
+  customerOrder = customerOrder.split(':'); //now Array
+  console.log(typeof customerOrder, customerOrder);
   
   // console.log(`You have ordered ${customerOrder}`);
   document.getElementById("orderRecap").innerHTML = `You have ordered ${customerOrder}`;
@@ -14,31 +19,79 @@ function orderRecapClear() {
   document.getElementById("orderRecap").innerHTML = "";
 }
 
-let menu = {
-  burger:100,
-  cheeseburger:100,
-  hotdog:100,
-  nuggets:1000,
-  fries:100,
-  tots:100,
-  shake:100,
-  soda:100,
-  water:100
-}
+// function pushData() {
+//   // get value from the input text
+//   var inputText = document.getElementById('inputText').value;
+  
+//   // append data to the array
+//   customerOrder.push(inputText);
+  
+//   var pval = "";
+  
+//   for(i = 0; i < customerOrder.length; i++)
+//   {
+//     pval = pval + customerOrder[i] + "<br>";
+//   }
+  
+//   // display array data
+//   console.log(customerOrder);
+//   document.getElementById('pText').innerHTML = pval;
 
-// function orderOptimize() {
-//   let res = customerOrder.split(" ");
-//   console.log(res);
+
+let menu = [
+  {'item':'burger', 'stock':100},
+  {'item':'cheeseburger', 'stock':100},
+  {'item':'hotdog', 'stock':100},
+  {'item':'nuggets', 'stock':1000},
+  {'item':'fries', 'stock':100},
+  {'item':'tots', 'stock':100},
+  {'item':'shake', 'stock':100},
+  {'item':'soda', 'stock':100},
+  {'item':'water', 'stock':100}
+]
+// console.log(typeof menu, menu);
+
+
+// function placeOrder() {
+//   let customerOrder = document.getElementById("orderInput").value;
+//   // console.log(customerOrder.trim());
+//   console.log(customerOrder.split(","));
+//   console.log(customerOrder.split(" ").join(""));
+//   console.log(customerOrder);
+  
+//   // console.log(`You have ordered ${customerOrder}`);
+//   document.getElementById("orderRecap").innerHTML = `You have ordered ${customerOrder}`;
+//   document.getElementById("orderInput").value = "";
 // }
 
-// let p1 = 5;
-// let p2 = 7;
-
-// document.getElementById("placeOrder").addEventListener("click", function() {
-//   myFunction(p1, p2);
-// });
-
-// function myFunction(a, b) {
-//   let result = a * b;
-//   document.getElementById("recap").innerHTML = `You have ordered ${result}`;
+// function orderRecapClear() {
+//   document.getElementById("orderRecap").innerHTML = "";
 // }
+
+// let menu = [
+//   {'item':'burger', 'stock':100},
+//   {'item':'cheeseburger', 'stock':100},
+//   {'item':'hotdog', 'stock':100},
+//   {'item':'nuggets', 'stock':1000},
+//   {'item':'fries', 'stock':100},
+//   {'item':'tots', 'stock':100},
+//   {'item':'shake', 'stock':100},
+//   {'item':'soda', 'stock':100},
+//   {'item':'water', 'stock':100}
+// ]
+// console.log(typeof menu, menu);
+
+// OR: (Which format best to use?)
+
+// let menu = {
+//   burger:100,
+//   cheeseburger:100,
+//   hotdog:100,
+//   nuggets:1000,
+//   fries:100,
+//   tots:100,
+//   shake:100,
+//   soda:100,
+//   water:100
+// }
+// console.log(typeof menu, menu);
